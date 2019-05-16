a9b52ccf7b97e2809b3087d49e11cfbf22e2cdab
Sarah B <smblair87@gmail.com>
2019-05-15 19:21:50 -0700

    Added routes

--- server.js
+++ server.js
@@ -19,6 +19,18 @@ app.use(express.json());
 // Routes
 // =============================================================
 
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
 
 // Starts the server to begin listening
 // =============================================================
