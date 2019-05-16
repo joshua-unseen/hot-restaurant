06a7656d66bea7015d9df2c99283c50a94cb37e9
joshua-unseen <corpselight@gmail.com>
2019-05-15 20:10:03 -0700

    fixed links in index

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/tables", function(req, res) {
+app.get("/view", function(req, res) {
   res.sendFile(path.join(__dirname, "view.html"));
 });
 
